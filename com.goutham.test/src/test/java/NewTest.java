15358f91f93fd561a629112b55da5ccb2a2deba5
gtmpai2 <122079256+gtmpai2@users.noreply.github.com>
2023-01-06 17:49:45 +0530

    asd

---
+++ com.goutham.test/src/test/java/NewTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("Test");
+	  
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
